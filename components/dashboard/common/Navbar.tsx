8984b3e2ed749f96f42f889bb93922e923886652
Thirana Embuldeniya <t.embuldeniya@gmail.com>
2025-09-20 12:45:29 +0530

    added side bar

--- components/dashboard/common/Navbar.tsx
+++ components/dashboard/common/Navbar.tsx
@@ -1,12 +1,13 @@
 import { ModeToggle } from '@/components/mode-toggler'
 import Link from 'next/link'
 import UserProfileIcon from '@/components/dashboard/UserProfileIcon'
+import { SidebarTrigger } from '@/components/ui/sidebar'
 
 const Navbar = () => {
     return (
         <nav className="p-4 flex items-center justify-between">
             {/*LEFT*/}
-            collapsbutton
+            <SidebarTrigger />
             {/*LEFT*/}
             <div className="flex items-center gap-8">
                 <Link href="/">Dashboard</Link>
